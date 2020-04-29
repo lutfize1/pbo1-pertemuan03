32c69a4ccb68d79f1f01fa2602faf0ccda952777
lutfize1 <lutfize1@gmail.com>
2020-04-29 14:08:19 +0700

    Percabangan2

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -7,8 +7,11 @@ public class IfElse {
 
             if(uangDiDompet<totalBelanja) {
                 System.out.println("uang kurang, kurangi jajan anda");
+            }else if(uangDiDompet>totalBelanja) {
+                double angusul = uangDiDompet - totalBelanja;
+                System.out.println("uang cukup, angsul : " + angusul);
             }else{
-                System.out.println("uang cukup, berelaan lah");
+                System.out.println("uang pas.. UwU..");
             }
         }
 }
