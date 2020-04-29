8159b11c92bfcb67ca2bfa5347260df52e7e70c0
lutfize1 <lutfize1@gmail.com>
2020-04-29 13:01:56 +0700

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    // operator (=) sebagai assignment/penugasan
+    // menempatkan nilai kedalam variabel
+    // baris 5-7 adalah comment/komentar yang tidal di jalankan oleh prgram
+
+    string nama = "Muhammad Lutfi";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+
+}
